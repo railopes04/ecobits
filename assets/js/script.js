0a311c51b589275967f9fc95de39c00464c415c8
Rai Lopes <129956131+railopes04@users.noreply.github.com>
2024-05-12 21:24:04 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,7 +25,7 @@ var swiper = new Swiper(".enderecoSwiper", {
    nextEl: ".endereco-button-next",
    prevEl: ".endereco-button-prev",
   },
-  slidesPerView: 1.5,
+  slidesPerView: 1.3,
   initialSlide: 0,
   loopedSlides: 3,
   visibilityFullFit: true,
@@ -37,8 +37,27 @@ var swiper = new Swiper(".enderecoSwiper", {
     spaceBetween: 25,
   },
   1024: {
-    slidesPerView: 4,
+    slidesPerView: 3,
     spaceBetween: 25,
   },
 }
 });
+
+//===============Back to top===============//
+// Mostra ou oculta o botão baseado na posição de rolagem
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  var button = document.getElementById("back-to-top");
+  var conteudoTopo = document.getElementById("bl-sobre").getBoundingClientRect().top;
+  
+  if (conteudoTopo < 0) {
+    button.style.display = "block";
+  } else {
+    button.style.display = "none";
+  }
+}
+// Faz a página rolar para o topo quando o botão é clicado
+function scrollToTop() {
+  document.body.scrollTop = 0; // Para Safari
+  document.documentElement.scrollTop = 0; // Para Chrome, Firefox, IE e Opera
+}
